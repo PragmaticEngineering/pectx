c4cbb888dbb001a002dd1b579b607bd0670b66ee
James Rhoat <james@rhoat.com>
2024-03-10 21:26:50 -0400

    adding constants for common keys that will be used

---
+++ consts.go
@@ -0,0 +1,10 @@
+package pectx
+
+const (
+	// RequestIDKey is the key used to store the request ID in the context.
+	RequestIDKey = "requestID"
+	// RequestMethodKey is the key used to store the request method in the context.
+	RequestMethodKey = "requestMethod"
+	// RequestPathKey is the key used to store the request path in the context.
+	RequestPathKey = "requestPath"
+)
